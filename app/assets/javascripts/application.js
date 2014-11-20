751f8ed73d22f220b427b185ee17fc7775c5c94d
Raul Vega <raulvprivate@gmail.com>
2014-11-20 16:27:48 +0100

    The error problem is fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,10 +22,36 @@ editor.getSession().setMode("ace/mode/javascript");
 
 var exeWindow = document.getElementById("exeWindow");
 var errorWindow = document.getElementById("errorWindow");
-var btnExe = document.getElementById("btn-exe");
-btnExe.addEventListener('click', execute, true);
+//its not making the changeAnnotation the second time
+editor.getSession().on("changeAnnotation", execute);
 
-function execute(){
+	function execute(){
+			try {
+				
+				exeWindow.innerHTML = eval(editor.getValue());
+				
+
+			} catch(err){
+				console.log('error',err);
+				errorWindow.innerHTML = "";
+				var annot = editor.getSession().getAnnotations();
+
+				for (var key in annot){
+				    if (annot.hasOwnProperty(key))
+				    	console.log(annot[key].text + "on line " + " " + (parseInt(annot[key].row)+1));
+				        errorWindow.innerHTML = annot[key].text + " Line " + " " + (parseInt(annot[key].row)+1);
+						exeWindow.innerHTML = "";
+				}
+
+			
+			}
+			if (TogetherJS.running) {
+			    TogetherJS.send({type: "execute"});
+			}
+  			
+		};
+
+/*function execute(){
 	
   		exeWindow.innerHTML = eval(editor.getValue());
   		errorWindow.innerHTML = "";
@@ -43,7 +69,7 @@ function execute(){
   	if (TogetherJS.running) {
 	    TogetherJS.send({type: "execute"});
 	}
-}
+}*/
 
 TogetherJS.hub.on("execute", function (msg) {
     if (! msg.sameUrl) {
